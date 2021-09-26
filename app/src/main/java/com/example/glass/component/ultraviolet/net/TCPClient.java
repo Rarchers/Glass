5a9c3e619230f25326bc1b995da5005802c1f0b4
Rarcher <2553263392@qq.com>
2021-09-26 12:43:59 +0800

    tcp 完善

--- app/src/main/java/com/example/glass/component/ultraviolet/net/TCPClient.java
+++ app/src/main/java/com/example/glass/component/ultraviolet/net/TCPClient.java
@@ -1,14 +1,47 @@
 package com.example.glass.component.ultraviolet.net;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class TCPClient {
+public class TCPClient extends Thread {
     public final static int CAMERA_DATA_PORT = 60003;
-    private String ServiceIP;
+
     private Socket socket;
+    private OnMsgReturnedListener listener;
 
-    public TCPClient(String serviceIP) {
-        ServiceIP = serviceIP;
+    public TCPClient(String serviceIP, OnMsgReturnedListener listener) {
+        this.listener = listener;
+        try {
+            socket = new Socket(serviceIP, CAMERA_DATA_PORT);
+        } catch (Exception e) {
+            listener.onError(e);
+        }
+    }
 
+    @SuppressWarnings("InfiniteLoopStatement")
+    @Override
+    public void run() {
+        super.run();
+
+            try {
+
+                if (socket == null){
+                    listener.onError(new Exception("Socket init error"));
+                    return;
+                }
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+                while (true) {
+                    String readline = br.readLine();
+                    if (readline != null) {
+                        listener.onMsgReturned(readline);
+                    }
+                }
+
+            } catch (IOException e) {
+                listener.onError(e);
+        }
     }
 }
