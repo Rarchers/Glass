180d16605c87c898a12425d0f16cc06bb49783ca
Rarcher <2553263392@qq.com>
2022-01-10 11:50:08 +0800

    视频组件API添加

--- app/src/main/java/com/example/glass/component/Config.java
+++ app/src/main/java/com/example/glass/component/Config.java
@@ -10,4 +10,11 @@ public class Config {
     public final static String remoteIP_PhoneUse =  "10.220.92.157";
     public final static int sendPort = 1234;
     public final static int receivePort = 1234;
+
+    //视频组件相关API
+    public final static String VIDEO_FETCH_PAINT = "http://59.67.246.92:8089/api/get_cfg";
+    public final static String VIDEO_VOICE_URL = "59.67.246.92";
+    public final static String VIDEO_VOICE_PORT = "8818";
+    public final static String VIDEO_CHECK_SUCCESS = "http://59.67.246.92:8088/live?app=live&stream=stream-1";
+    public final static String VIDEO_PUSH = "rtmp://59.67.246.92/live/stream-1";
 }
